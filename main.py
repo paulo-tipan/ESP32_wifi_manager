c32b25d9a32125867939329e4bd59df7278b5f6c
paulo-tipan <110475262+paulo-tipan@users.noreply.github.com>
2022-09-06 22:42:43 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from wifi_manager import WifiManager
+import utime
+# Example of usage
+
+wm = WifiManager()
+wm.connect()
+
+while True:
+    if wm.is_connected():
+        print('Connected!')
+    else:
+        print('Disconnected!')
+    utime.sleep(10)
